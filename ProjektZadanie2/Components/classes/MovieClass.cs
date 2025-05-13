af71ce1af2b28f51fc4de9f46c5ebe1e5e3d103d
bwlodarc <b748462847@gmail.com>
2025-05-14 00:26:24 +0200

    Zadanie 2 ukończone

--- ProjektZadanie2/Components/classes/MovieClass.cs
+++ ProjektZadanie2/Components/classes/MovieClass.cs
@@ -4,7 +4,7 @@ namespace ProjektZadanie2.Data.Models;
 public class Movie
 {
     public int Id { get; set; }
-    [Required]
+    [Required(ErrorMessage = "Title is required!")]
     public string? Title { get; set; } // "?" oznacza że może być null
     public string? Description { get; set; }
     public int? ReleaseYear { get; set; } // int? oznacza, że może być null
@@ -12,7 +12,17 @@ public class Movie
     [DataType(DataType.Date)]
     public DateTime? ReleaseDate { get; set; }
     public double? Rate { get; set; }
-    
     public int RateNumbers { get; private set; }
+    [DataType(DataType.ImageUrl)]
+    public string? ImageUrl {get; set;}
+
     public void IncreaseRateNumbers() {++RateNumbers;}
+    public void AddRating(int newRating) {
+        // Walidacja przedzdziału od 1 do 10 jest w Details.razor
+        double currentTotal = (Rate ?? 0) * RateNumbers;
+        double newTotal = currentTotal + newRating;
+        ++RateNumbers;
+        if(RateNumbers > 0) Rate = newTotal / RateNumbers;
+        else Rate = null; // Nie dzielimy przez 0
+    }
 }
